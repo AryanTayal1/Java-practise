2e79693dfabdb957215c0f1ea478f6eeb596c334
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-16 02:49:59 +0530

    Solved que1

--- conditionalloops/que1.java
+++ conditionalloops/que1.java
@@ -1,6 +1,15 @@
 //Subtract the Product and Sum of Digits of an Integer
 package conditionalloops;
-
+import java.util.Scanner;
 public class que1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input num1:");
+        int num1 = sc.nextInt();
+        System.out.print("Input num2:");
+        int num2 = sc.nextInt();
+        int a=(num1*num2)-(num2+num1);
+        System.out.print("Answer:"+a);
+    }
     
 }
