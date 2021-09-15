47c09f4937af5c05a15bac65f1f0141a475f87f4
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-16 01:46:20 +0530

    Total surface area of cube added

--- conditionalloops/TSA_of_cube.java
+++ conditionalloops/TSA_of_cube.java
@@ -1,5 +1,10 @@
 package conditionalloops;
 import java.util.Scanner;
 public class TSA_of_cube {
-    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Side:");
+        int a=sc.nextInt();
+        System.out.print("Total Surface Area of Cube:"+6*a*a);
+    }
 }
