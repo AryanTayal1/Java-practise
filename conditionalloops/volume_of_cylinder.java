66e4e9c1f89bc41b12477f4cdac3818ee87f6b0b
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-16 01:30:25 +0530

    Volume of Cylinder Added

--- conditionalloops/volume_of_cylinder.java
+++ conditionalloops/volume_of_cylinder.java
@@ -1,5 +1,13 @@
 package conditionalloops;
 import java.util.Scanner;
 public class volume_of_cylinder {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Radius:");
+        int r = sc.nextInt();
+        System.out.print("Height:");
+        int h = sc.nextInt();
+        float v=((22/7f)*r*r*h);
+        System.out.print("Volume of Cylinder:"+v);
+    }
 }
