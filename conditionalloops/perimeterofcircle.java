d01cf26e2a94fd311184eb247e53b8a0e8b2905e
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-15 16:08:45 +0530

    Area of circle calculated

--- conditionalloops/perimeterofcircle.java
+++ conditionalloops/perimeterofcircle.java
@@ -1,5 +1,12 @@
 package conditionalloops;
+import java.util.Scanner;
 
 public class perimeterofcircle {
-    
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Radius:");
+        int r=sc.nextInt();
+        float a=r*r*22/7f;
+        System.out.println("Area of Circle :"+a);
+    }
 }
