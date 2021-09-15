f7b030be4ab3626270c13960aba4014fac2fcce9
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-16 01:24:27 +0530

    Volume of Prism Solved

--- conditionalloops/volume_of_prism.java
+++ conditionalloops/volume_of_prism.java
@@ -1,5 +1,13 @@
 package conditionalloops;
 import java.util.Scanner;
 public class volume_of_prism {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Area of base:");
+        int b=sc.nextInt();
+        System.out.print("Height:");
+        int h=sc.nextInt();
+        System.out.println("Volume of Prism is:"+b*h);
+    }
     
 }
