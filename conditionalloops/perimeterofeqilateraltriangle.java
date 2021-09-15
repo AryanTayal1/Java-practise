5dcb14d448b010e2530667680dc50f29aa6d8d48
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-15 16:19:02 +0530

    Perimeter of Equilateral Triangle Calculated

--- conditionalloops/perimeterofeqilateraltriangle.java
+++ conditionalloops/perimeterofeqilateraltriangle.java
@@ -1,5 +1,13 @@
 package conditionalloops;
+import java.util.Scanner;
 
 public class perimeterofeqilateraltriangle {
-    
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        //Taking input of the side of triangle
+        System.out.print("Length of side");
+        int a=sc.nextInt();
+        //Perimeter of Triangle is 3*side of equilateral triangle
+        System.out.println("Perimeter:"+3*a);
+    }
 }
