eddc847d65e29f6ef57bd0dabf1f28a8777665a5
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-15 16:11:54 +0530

    Updated

--- conditionalloops/perimeterofcircle.java
+++ conditionalloops/perimeterofcircle.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 public class perimeterofcircle {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
+        //Taking input of radius of circle
         System.out.print("Radius:");
         int r=sc.nextInt();
+        // Calculated the perimeter which is 2*pie*r which comes out to be r*44/7
         float a=r*44/7f;
         System.out.println("Area of Circle :"+a);
     }
