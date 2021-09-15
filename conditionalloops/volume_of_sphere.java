07d9a17b7f4fd941025a9dde3389065914ec3569
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-16 01:34:53 +0530

    Volume of sphere added

--- conditionalloops/volume_of_sphere.java
+++ conditionalloops/volume_of_sphere.java
@@ -1,5 +1,12 @@
 package conditionalloops;
 import java.util.Scanner;
 public class volume_of_sphere {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Radius:");
+        int r = sc.nextInt();
+        float v=((88/21f)*r*r*r);
+        System.out.println("Volume of Sphere:"+v);
+    }
     
 }
