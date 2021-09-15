32b03cfbf554775ad605cd8112f988dd1f3310ff
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-16 01:07:38 +0530

    Perimeter of rohombus added

--- conditionalloops/perimeterofrhombus.java
+++ conditionalloops/perimeterofrhombus.java
@@ -1,5 +1,10 @@
 package conditionalloops;
 import java.util.Scanner;
 public class perimeterofrhombus {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Side: ");
+        int x=sc.nextInt();
+        System.out.println("Perimeter of rhombus:"+4*x);
+    }
 }
