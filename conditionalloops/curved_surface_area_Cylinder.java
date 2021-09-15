cc055c728500164e155f51336b7b2d2b71bce907
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-16 01:52:11 +0530

    Curved surface area of cylunder solved

--- conditionalloops/curved_surface_area_Cylinder.java
+++ conditionalloops/curved_surface_area_Cylinder.java
@@ -1,5 +1,14 @@
 package conditionalloops;
 import java.util.Scanner;
 public class curved_surface_area_Cylinder {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Radius:");
+        int r = s.nextInt();
+        System.out.print("Height:");
+        int h = s.nextInt();
+        float v=((44/7f)*r*h);
+        System.out.println("Curved Surface Area of Cylinder:"+v);
+    }
     
 }
