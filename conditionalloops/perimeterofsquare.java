89fc079d1f96359f2c6f3ceccc235f232f8954a4
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-16 01:02:24 +0530

    Perimeter of aquare added

--- conditionalloops/perimeterofsquare.java
+++ conditionalloops/perimeterofsquare.java
@@ -1,5 +1,11 @@
 package conditionalloops;
 import java.util.Scanner;
 public class perimeterofsquare {
-    
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Side: ");
+        int a=sc.nextInt();
+        System.out.println("Perimeter:"+4*a);
+
+    }    
 }
