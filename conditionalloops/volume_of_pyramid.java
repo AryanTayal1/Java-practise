a6555d2a185ac04dcdac55fe1609cc05705a0e25
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-16 01:41:02 +0530

    Volume of pyramid added

--- conditionalloops/volume_of_pyramid.java
+++ conditionalloops/volume_of_pyramid.java
@@ -1,5 +1,16 @@
 package conditionalloops;
 import java.util.Scanner;
 public class volume_of_pyramid {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Base Length:");
+        int b=sc.nextInt();
+        System.out.print("Base Width:");
+        int w=sc.nextInt();
+        System.out.print("Height:");
+        int h=sc.nextInt();
+        float v=(b*w*h/3f);
+        System.out.print("Volume of Pyramid:"+v);
+    }
     
 }
