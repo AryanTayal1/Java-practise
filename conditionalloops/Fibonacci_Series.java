0c66248bf7f1ae243dbee81451865bcc92c12dc5
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-16 00:58:09 +0530

    Perimeter of rectangle solved

--- conditionalloops/Fibonacci_Series.java
+++ conditionalloops/Fibonacci_Series.java
@@ -1,5 +1,9 @@
 package conditionalloops;
 import java.util.Scanner;
 public class Fibonacci_Series {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+    }
     
 }
--- conditionalloops/perimeterofrectangle.java
+++ conditionalloops/perimeterofrectangle.java
@@ -1,5 +1,13 @@
 package conditionalloops;
 import java.util.Scanner;
 public class perimeterofrectangle {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Breath:");
+        int a=sc.nextInt();
+        System.out.print("Length:");
+        int b=sc.nextInt();
+        System.out.println("Area:"+(2*(a+b)));
+    }
     
 }
