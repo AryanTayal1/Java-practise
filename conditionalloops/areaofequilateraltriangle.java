7d6d5e1520076624a41ae212b7b0b7560c604d3a
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-15 15:58:42 +0530

    Area of equilatera triangle calculated

--- conditionalloops/areaofequilateraltriangle.java
+++ conditionalloops/areaofequilateraltriangle.java
@@ -1,5 +1,12 @@
 package conditionalloops;
-
+import java.util.Scanner;
 public class areaofequilateraltriangle {
-    
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the side:");
+        int a=sc.nextInt();
+        float b= (a*a*1.74f/4);
+        System.out.println("Area:"+b);
+    }
+        
 }
