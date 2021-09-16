a13724da5d59de3b4a4bf0e06b346868aa36325e
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-16 14:36:22 +0530

    All questions solved

--- conditionalloops/que3.java
+++ conditionalloops/que3.java
@@ -2,7 +2,19 @@
 package conditionalloops;
 import java.util.Scanner;
 public class que3 {
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-    }
+    public static void main(String[] args) {
+        int n;
+        int x=0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input an integer");
+       
+        while ((n = input.nextInt()) != 0) {
+          System.out.print("You entered " + n);
+          System.out.print("Input an integer");
+          x=x+n;
+        }
+       
+        System.out.println("Out of loop");
+        System.out.println("Sum of all integers is:"+x);
+      }
 }
--- conditionalloops/que4.java
+++ conditionalloops/que4.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 public class que4 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int n;
+        int x=0;
+        System.out.print("Input an integer ");
+       
+        while ((n = sc.nextInt()) != 0) {
+          System.out.print("You entered " + n);
+          System.out.print("Input an integer ");
+          if(n>x){
+              x=n;
+              continue;
+          }
+        }
+        System.out.println("Out of loop");
+        System.out.println("Largest of all integers is:"+x);
         
     }    
 }
