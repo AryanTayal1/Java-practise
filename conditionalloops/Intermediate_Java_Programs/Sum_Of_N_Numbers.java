e171641e5bb3dd0fbbfa190520b8ee30a1fad100
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-17 14:47:31 +0530

    Sum of n numbers calculated

--- conditionalloops/Intermediate_Java_Programs/Sum_Of_N_Numbers.java
+++ conditionalloops/Intermediate_Java_Programs/Sum_Of_N_Numbers.java
@@ -1,5 +1,16 @@
 package conditionalloops.Intermediate_Java_Programs;
-
+import java.util.Scanner;
 public class Sum_Of_N_Numbers {
-    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("No. of numbers:");
+        int n=sc.nextInt();
+        int j=0;
+        for (int i=0;i<=n;i++){
+            System.out.print("Input number:");
+            int d=sc.nextInt();
+            j=j+d;
+        }
+        System.out.println("Sum:"+j);
+    }
 }
