d2a8fb262fe915b0229fe7426db053687830e658
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-18 03:48:18 +0530

    Vowels and consonants problem solved

--- conditionalloops/Intermediate_Java_Programs/Vowels_Consonants.java
+++ conditionalloops/Intermediate_Java_Programs/Vowels_Consonants.java
@@ -1,5 +1,24 @@
 package conditionalloops.Intermediate_Java_Programs;
-
+import java.util.Scanner;
 public class Vowels_Consonants {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input a String: ");
+        String a=sc.nextLine();
+        int v=0,c=0;
+        a=a.toLowerCase();
+        for(int i=0;i<a.length();i++) {
+            char ch=a.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+                v++;
+            }
+            else if((ch>='a'&&ch<='z')){
+                c++;
+            }
+        }
+        System.out.println("number of Vowels:" +v);
+        System.out.println("number of Consonants:" +c);
+
+    }
     
 }
