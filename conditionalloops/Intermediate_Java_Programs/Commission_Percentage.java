212dbcd0be3d87eefe8d30d5c2515870694ff0d3
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-17 14:06:33 +0530

    comission percentage calculated

--- conditionalloops/Intermediate_Java_Programs/Commission_Percentage.java
+++ conditionalloops/Intermediate_Java_Programs/Commission_Percentage.java
@@ -1,5 +1,14 @@
 package conditionalloops.Intermediate_Java_Programs;
-
+import java.util.Scanner;
 public class Commission_Percentage {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input the amount:");
+        int amount = sc.nextInt();
+        System.out.print("Commission Amount:");
+        int commission=sc.nextInt();
+        float cp=commission*100f/amount;
+        System.out.println("Commission Percentage:"+cp+"%");
+    }
     
 }
