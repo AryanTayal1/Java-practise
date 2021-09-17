a5616ef7ebeb6977d86bb9f88e4bac8b34ba382f
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-17 14:40:14 +0530

    Calculated CGPA

--- conditionalloops/Intermediate_Java_Programs/Calculate_CGPA.java
+++ conditionalloops/Intermediate_Java_Programs/Calculate_CGPA.java
@@ -1,5 +1,18 @@
 package conditionalloops.Intermediate_Java_Programs;
-
+import java.util.Scanner;
 public class Calculate_CGPA {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("No.of subjects:");
+        int n=sc.nextInt();
+        int j=0;
+        for (int i =0;i<=n;i++){
+            System.out.print("Input marks out of 10:");
+            int d=sc.nextInt();
+            j=j+d;
+        }
+        double b=j/n;
+        System.out.println("CGPA:"+b);
+    }
     
 }
