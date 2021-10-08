c2180c6329ca31c89ff88c55d85cefc5b8edd44a
Aryan Tayal <tayalaryan15@gmail.com>
2021-10-08 13:45:55 +0530

    Questions solved

--- functions/ques2.java
+++ functions/ques2.java
@@ -1,10 +1,24 @@
 //Define a program to find out whether a given number is even or odd.
 package functions;
+import java.util.Scanner;
 public class ques2 {
     public static void main(String args[]){
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input NUmber:");
+        int num = scanner.nextInt();
+        String ans=evenorodd(num);
+        System.out.println("Number is "+ans);
     }
-    static String evenorodd{
+    static String evenorodd(int num) {
+        int r=num%2;
+        String ans;
+        if(r==0){
+            ans="even";
+        }
+        else{
+            ans="odd";
+        }
+        return ans;
         
     }
     
--- functions/ques3.java
+++ functions/ques3.java
@@ -1,6 +1,22 @@
 //A person is eligible to vote if his/her age is greater than or equal to 18. Define a method to find out if he/she is eligible to vote.
 package functions;
-
+import java.util.Scanner;
 public class ques3 {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input Age");
+        int age = sc.nextInt();
+        String ans=eligibility(age);
+        System.out.println("Person is "+ans);
+    }
+    static String eligibility(int age){
+        String ans;
+        if (age<18){
+            ans="Not eligible to vote";
+        }
+        else{
+            ans="eligible to vote";
+        }
+        return ans;
+    }
 }
