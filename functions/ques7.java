301890e668d765db7372bd87bb7d13046f7ffa6c
Aryan Tayal <tayalaryan15@gmail.com>
2021-10-12 02:45:22 +0530

    ques7 solved

--- functions/ques7.java
+++ functions/ques7.java
@@ -6,10 +6,27 @@ public class ques7 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input number: ");
         int num = scanner.nextInt();
+        boolean ans=primeornot( num);
+        System.out.print(ans);
+
     }
-    static int primeornot(int num){
-        int ans;
+    static boolean primeornot(int num){
         
-        return ans;
+        if(num<=1){
+           return false;
+        }
+        else if(num==2){
+            return true;
+        }
+        else if(num%2==0){
+            return false;
+        }
+        for (int i = 3; i <= Math.sqrt(num); i += 2)
+        {
+            if (num % i == 0)
+                return false;
+        }
+        
+        return true;
     }
 }
