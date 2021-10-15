8e5b553664ec47331e2d038205347cb20a15eaec
Aryan Tayal <tayalaryan15@gmail.com>
2021-10-15 21:10:40 +0530

    ques10 solved

--- functions/ques10.java
+++ functions/ques10.java
@@ -4,7 +4,27 @@ import java.util.Scanner;
 
 public class ques10 {
     public static void main(String[] args){
-        
+        Scanner scanner = new Scanner(System.in);
+        int n= scanner.nextInt();
+        String ans=palindrome(n);
+        System.out.println(ans);
+    }
+    static String palindrome(int n){
+        int temp,sum=0,r;
+        String ans;
+        temp=n;    
+        while(n>0){    
+            r=n%10;  //getting remainder  
+            sum=(sum*10)+r;    
+            n=n/10;    
+        }    
+        if(temp==sum)    
+        ans="Palindrome number ";    
+        else    
+        ans="Not Palindrome number";    
+
+        return ans;
+
     }
     
 }
